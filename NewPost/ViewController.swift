2924f1cd3816ae18dff4df408975c65b50507bfa
Justin <bin@FBIs-MacBook-Air.local>
2014-10-17 14:14:19 -0500

    Initial Commit

---
+++ NewPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewPost
+//
+//  Created by Justin on 10/17/14.
+//  Copyright (c) 2014 Justin Gondeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
